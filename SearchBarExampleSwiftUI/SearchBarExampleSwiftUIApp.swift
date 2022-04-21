75096cf6fd4083a4166fc2fbe5621f897e6ba96b
littleossa <littleossa@gmail.com>
2022-04-21 21:21:36 +0900

    Initial Commit

---
+++ SearchBarExampleSwiftUI/SearchBarExampleSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchBarExampleSwiftUIApp.swift
+//  SearchBarExampleSwiftUI
+//
+//  Created by 平岡修 on 2022/04/21.
+//
+
+import SwiftUI
+
+@main
+struct SearchBarExampleSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
